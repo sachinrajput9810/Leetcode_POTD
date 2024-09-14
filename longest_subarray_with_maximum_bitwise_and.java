b78898eee55b234ef61e8459d93c59accd6cdb11
Sachin Rajput <165654923+sachinrajput9810@users.noreply.github.com>
2024-09-14 18:09:52 +0530

    Longest subarray with bitwise AND

---
+++ longest_subarray_with_maximum_bitwise_and.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int max = -1 ;
+        for(int val : nums) max = Math.max(max , val) ;
+        int i = 0 ;
+        int n = nums.length ;
+        int len = 0 ;
+        int ans = 0 ;
+        while(i < n){
+            int val = nums[i] ;
+            if(val == max){
+                len += 1 ;
+                ans = Math.max(ans , len) ;
+            } 
+            else{
+                len = 0 ;
+            }
+            i++ ;
+        }
+        return ans ;
+    }
+}
