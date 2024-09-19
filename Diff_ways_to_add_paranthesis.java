60f223d377ba2fbf58066a4d3dbece33df79e623
Sachin Rajput <165654923+sachinrajput9810@users.noreply.github.com>
2024-09-19 14:37:43 +0530

    Leetcode Potd 19-09-2024

---
+++ Diff_ways_to_add_paranthesis.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> diffWaysToCompute(String expression) {
+        return solve(expression) ;
+    }
+    public List<Integer> solve(String exp){
+        int n = exp.length() ;
+        List<Integer> res = new ArrayList<>() ;
+        for(int i=0 ; i<n ; i++){
+            if(exp.charAt(i) == '+' || exp.charAt(i) == '-' || exp.charAt(i) == '*'){
+                List<Integer> left = solve(exp.substring(0,i)) ;
+                List<Integer> right = solve(exp.substring(i+1)) ;
+                for(int val1 : left){
+                    for(int val2 : right){
+                        if(exp.charAt(i) == '+'){
+                            res.add(val1+val2) ;
+                        }
+                        else if(exp.charAt(i) == '-'){
+                            res.add(val1 - val2) ;
+                        }
+                        else res.add(val1*val2) ;
+                    }
+                }
+            }
+        }
+        if(res.size() == 0) res.add(Integer.parseInt(exp)) ;
+        return res ;
+    }
+}
